dffb564184177eebc40c95471773e4a2307d929a
Marcos Carvalho <gc_marcos@hotmail.com>
2024-01-04 20:57:47 -0300

    Update ValidacoesListaTests.cs

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -38,7 +38,6 @@ public class ValidacoesListaTests
         var resultado = _validacoes.ListaContemDeterminadoNumero(lista, numeroParaProcurar);
 
         Assert.False(resultado);
-
     }
 
     [Fact]
